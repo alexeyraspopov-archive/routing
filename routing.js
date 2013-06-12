acf800caacf52377a031d86b31b7e8f9214eb440
Avenger7x <avenger7x13@gmail.com>
2013-06-12 13:50:03 +0300

    initial commit

---
+++ routing.js
@@ -0,0 +1,48 @@
+(function(factory){
+	'use strict';
+/*
+	TODO: use params object for route keys
+	TODO: listen 'hashchange'
+*/
+var routes = [];
+
+function route(path, callback){
+	routes.push({
+		path: compile(path),
+		callback: callback
+	});
+	// check?
+}
+
+function compile(path){
+	path = path.replace(/\/?$/, '/?').replace(/\/:([^\/]+)/g, '\/([^\/]+)').replace(/\*/g, '.+');
+
+	return new RegExp('^' + path + '$');
+}
+
+function check(){
+	var index, route, hash = document.location.hash.slice(1);
+
+	for(index = 0; index < routes.length; index++){
+		route = routes[index];
+
+		if(route.path.test(hash)){
+			route.callback.apply(null, route.path.exec(hash).slice(1));
+		}
+	}
+}
+
+factory('route', route);
+factory('check', check);
+
+})(function(name, object){
+	if(typeof define === 'function' && define.amd){
+		define(function(){
+			return object;
+		});
+	}else if(typeof window === 'object'){
+		window[name] = object;
+	}else{
+		module.exports = object;
+	}
+});
---
+++ spec/routing-spec.js
@@ -0,0 +1,5 @@
+describe('routing', function(){
+	it('should pass', function(){
+		expect(true).toBe(true);
+	});
+});
